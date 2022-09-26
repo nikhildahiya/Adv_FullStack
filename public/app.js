b3059e00457f6ca6fe6d84c48373b5a7bcab4245
Nikhil Dahiya <nikkdahiya@Nikhils-MacBook-Pro.local>
2022-09-26 19:01:06 -0400

    week4_part1

--- public/app.js
+++ public/app.js
@@ -3,88 +3,81 @@ const IssueFilter = () => {
 };
 
 const IssueRow = props => {
-  const rowStyle = {
-    border: "1px solid"
-  };
-  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Id), /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Title), /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Status), /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Owner), /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Created), /*#__PURE__*/React.createElement("td", {
-    style: props.rowStyle
-  }, props.Due));
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, props.Id), /*#__PURE__*/React.createElement("td", null, props.Status), /*#__PURE__*/React.createElement("td", null, props.Owner), /*#__PURE__*/React.createElement("td", null, props.Effort), /*#__PURE__*/React.createElement("td", null, props.Title), /*#__PURE__*/React.createElement("td", null, props.Created.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Due.toDateString()));
 };
 
 const IssueTable = ({
   issues
 }) => {
-  // const tempIssues = [
-  //   { Id: 1, Status:"Assigned", Owner:"Person-A", Created: new Date("2022-09-01"), Due: new Date("2022-09-05"), Title: "This is the First Issue" },
-  //   { Id: 2, Status:"Resolved", Owner:"Person-B", Created: new Date("2022-09-02"), Due: new Date("2022-09-06"), Title: "This is the Second Issue" },
-  // ];
-  //const newIssues = { Status: "Assigned", Owner:"Person-C", Created: new Date("2022-09-03"), Due: new Date("2022-09-07"), Title: "This is the Third Issue"};
-  // const [issues, setIssues] = React.useState(tempIssues);
-  // setTimeout(()=>{
-  //   setIssues(tempIssues);
-  // },1000)
-  // this is to add the value to temp array after delay and goes in the infilite loop
-  // setTimeout(()=>{
-  //   tempIssues.push(newIssues);
-  //   setIssues(tempIssues);
-  // },3000)
-  // let counter = 0;
-  // React.useEffect(()=> {
-  //   console.log("Hello", counter);
-  //   //add single issue to stater variable with 1 sec delay.
-  //   setTimeout(() =>{
-  //     AddSingleIssue(newIssues);
-  //   },1000)
-  //   counter++;
-  //If the [] these are empty so there is no array and it will add just add one entry
-  //if there is an array it will constantly keep refreshing the loop and adding items in array.
-  // },[]);
-  // const AddSingleIssue = (newIssues) => {
-  //   newIssues.Id = issues.length + 1;
-  //   let IssueList = issues.slice(); //Slice is used to get a new array in every entry which is added.
-  //   IssueList.push(newIssues);
-  //   console.log(IssueList);
-  //   setIssues(IssueList);
-  // }
   const IssueRows = issues.map(issue => /*#__PURE__*/React.createElement(IssueRow, {
     key: issue.Id,
     Id: issue.Id,
     Status: issue.Status,
     Owner: issue.Owner,
-    Created: issue.Created.toDateString(),
-    Due: issue.Due.toDateString(),
+    Effort: issue.Effort,
+    Created: issue.Created,
+    Due: issue.Due,
     Title: issue.Title
   }));
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Issue Table"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Id"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Title"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Due"))), /*#__PURE__*/React.createElement("tbody", null, IssueRows)));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Issue Table"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Id"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Title"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Due"))), /*#__PURE__*/React.createElement("tbody", null, IssueRows)));
 };
 
 const AddIssue = ({
   AddSingleIssue
 }) => {
-  const newIssues = {
-    Status: "Assigned",
-    Owner: "Person-C",
-    Created: new Date("2022-09-03"),
-    Due: new Date("2022-09-07"),
-    Title: "This is the Third Issue"
-  };
-  React.useEffect(() => {
-    console.log("Hello");
-    setTimeout(() => {
-      AddSingleIssue(newIssues);
-    }, 1000);
-  }, []);
-  return /*#__PURE__*/React.createElement("h1", null, "Add Issue");
+  // const newIssues = { Status: "Assigned", Owner:"Person-C", Created: new Date("2022-09-03"), Due: new Date("2022-09-07"), Title: "This is the Third Issue"};
+  // React.useEffect(()=> {
+  //   console.log("Hello");
+  //   setTimeout(() =>{
+  //     AddSingleIssue(newIssues);
+  //   },1000)  
+  // },[]);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let form = document.forms.addForm;
+    console.log(form);
+    let newIssues = {
+      Status: form.Status.value,
+      Owner: form.Owner.value,
+      Effort: form.Effort.value,
+      Created: new Date(form.Created.value),
+      Due: new Date(form.Due.value),
+      Title: form.Title.value
+    };
+    AddSingleIssue(newIssues);
+    form.reset();
+  }
+
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "This is Placeholder for Addissue"), /*#__PURE__*/React.createElement("form", {
+    name: "addForm",
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Status",
+    placeholder: "Status"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Owner",
+    placeholder: "Owner"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Effort",
+    placeholder: "Effort"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "date",
+    name: "Created",
+    placeholder: "Created"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "date",
+    name: "Due",
+    placeholder: "Due"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Title",
+    placeholder: "Title"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Submit")));
 };
 
 const IssueList = () => {
@@ -92,6 +85,7 @@ const IssueList = () => {
     Id: 1,
     Status: "Assigned",
     Owner: "Person-A",
+    Effort: 10,
     Created: new Date("2022-09-01"),
     Due: new Date("2022-09-05"),
     Title: "This is the First Issue"
@@ -99,6 +93,7 @@ const IssueList = () => {
     Id: 2,
     Status: "Resolved",
     Owner: "Person-B",
+    Effort: 20,
     Created: new Date("2022-09-02"),
     Due: new Date("2022-09-06"),
     Title: "This is the Second Issue"
--- src/app.jsx
+++ src/app.jsx
@@ -3,61 +3,22 @@ const IssueFilter = () => {
 };
 
 const IssueRow = (props) => {
-  const rowStyle = { border: "1px solid" };
   return (
     <tr>
-      <td style={props.rowStyle}>{props.Id}</td>
-      <td style={props.rowStyle}>{props.Title}</td>
-      <td style={props.rowStyle}>{props.Status}</td>
-      <td style={props.rowStyle}>{props.Owner}</td>
-      <td style={props.rowStyle}>{props.Created}</td>
-      <td style={props.rowStyle}>{props.Due}</td>
+      <td>{props.Id}</td>
+      <td>{props.Status}</td>
+      <td>{props.Owner}</td>
+      <td>{props.Effort}</td>
+      <td>{props.Title}</td>
+      <td>{props.Created.toDateString()}</td>
+      <td>{props.Due.toDateString()}</td>
     </tr>
   );
 };
 
 const IssueTable = ({issues}) => {
-
-  // const tempIssues = [
-  //   { Id: 1, Status:"Assigned", Owner:"Person-A", Created: new Date("2022-09-01"), Due: new Date("2022-09-05"), Title: "This is the First Issue" },
-  //   { Id: 2, Status:"Resolved", Owner:"Person-B", Created: new Date("2022-09-02"), Due: new Date("2022-09-06"), Title: "This is the Second Issue" },
-  // ];
-
-  //const newIssues = { Status: "Assigned", Owner:"Person-C", Created: new Date("2022-09-03"), Due: new Date("2022-09-07"), Title: "This is the Third Issue"};
-  // const [issues, setIssues] = React.useState(tempIssues);
-
-  // setTimeout(()=>{
-  //   setIssues(tempIssues);
-  // },1000)
-
-  // this is to add the value to temp array after delay and goes in the infilite loop
-  // setTimeout(()=>{
-  //   tempIssues.push(newIssues);
-  //   setIssues(tempIssues);
-  // },3000)
-
-  // let counter = 0;
-  // React.useEffect(()=> {
-  //   console.log("Hello", counter);
-  //   //add single issue to stater variable with 1 sec delay.
-  //   setTimeout(() =>{
-  //     AddSingleIssue(newIssues);
-  //   },1000)
-  //   counter++;
-    //If the [] these are empty so there is no array and it will add just add one entry
-  //if there is an array it will constantly keep refreshing the loop and adding items in array.
-  // },[]);
-
-  // const AddSingleIssue = (newIssues) => {
-  //   newIssues.Id = issues.length + 1;
-  //   let IssueList = issues.slice(); //Slice is used to get a new array in every entry which is added.
-  //   IssueList.push(newIssues);
-  //   console.log(IssueList);
-  //   setIssues(IssueList);
-  // }
-
   const IssueRows = issues.map((issue) => (
-    <IssueRow key={issue.Id} Id={issue.Id} Status={issue.Status} Owner={issue.Owner} Created={issue.Created.toDateString()} Due={issue.Due.toDateString()} Title={issue.Title} />
+    <IssueRow key={issue.Id} Id={issue.Id} Status={issue.Status} Owner={issue.Owner} Effort={issue.Effort} Created={issue.Created} Due={issue.Due} Title={issue.Title} />
   ));
   return (
     <div>
@@ -68,6 +29,7 @@ const IssueTable = ({issues}) => {
             <th >Id</th>
             <th >Status</th>
             <th>Owner</th>
+            <th>Effort</th>
             <th>Title</th>
             <th>Created</th>
             <th>Due</th>
@@ -80,20 +42,51 @@ const IssueTable = ({issues}) => {
 };
 
 const AddIssue = ({AddSingleIssue}) => {
-  const newIssues = { Status: "Assigned", Owner:"Person-C", Created: new Date("2022-09-03"), Due: new Date("2022-09-07"), Title: "This is the Third Issue"};
-  React.useEffect(()=> {
-    console.log("Hello");
-    setTimeout(() =>{
-      AddSingleIssue(newIssues);
-    },1000)  
-  },[]);
-  return <h1>Add Issue</h1>;
+   // const newIssues = { Status: "Assigned", Owner:"Person-C", Created: new Date("2022-09-03"), Due: new Date("2022-09-07"), Title: "This is the Third Issue"};
+  // React.useEffect(()=> {
+  //   console.log("Hello");
+  //   setTimeout(() =>{
+  //     AddSingleIssue(newIssues);
+  //   },1000)  
+  // },[]);
+
+  function handleSubmit(event){
+    event.preventDefault();
+    let form = document.forms.addForm;
+    console.log(form);
+    let newIssues = {
+      Status: form.Status.value,
+      Owner: form.Owner.value,
+      Effort: form.Effort.value,
+      Created: new Date(form.Created.value),
+      Due: new Date(form.Due.value),
+      Title: form.Title.value
+    }
+    
+    AddSingleIssue(newIssues);
+    form.reset();
+  }
+
+  return(
+  <div>
+    <h2>This is Placeholder for Addissue</h2>
+    <form name="addForm" onSubmit={handleSubmit}>
+      <input type ="text" name="Status" placeholder="Status"></input>
+      <input type ="text" name="Owner" placeholder="Owner"></input>
+      <input type ="text" name="Effort" placeholder="Effort"></input>
+      <input type ="date" name="Created" placeholder="Created"></input>
+      <input type ="date" name="Due" placeholder="Due"></input>
+      <input type ="text" name="Title" placeholder="Title"></input>
+      <button type="submit">Submit</button>
+
+    </form>
+  </div>)
 };
 
 const IssueList= () => {
   const tempIssues = [
-    { Id: 1, Status:"Assigned", Owner:"Person-A", Created: new Date("2022-09-01"), Due: new Date("2022-09-05"), Title: "This is the First Issue" },
-    { Id: 2, Status:"Resolved", Owner:"Person-B", Created: new Date("2022-09-02"), Due: new Date("2022-09-06"), Title: "This is the Second Issue" },
+    { Id: 1, Status:"Assigned", Owner:"Person-A", Effort:10, Created: new Date("2022-09-01"), Due: new Date("2022-09-05"), Title: "This is the First Issue" },
+    { Id: 2, Status:"Resolved", Owner:"Person-B", Effort:20,Created: new Date("2022-09-02"), Due: new Date("2022-09-06"), Title: "This is the Second Issue" },
   ];
 
   const [issues, setIssues] = React.useState(tempIssues);
@@ -105,7 +98,6 @@ const IssueList= () => {
     console.log(IssueList);
     setIssues(IssueList);
   }
-
   return(
     <div>
     <IssueFilter />
